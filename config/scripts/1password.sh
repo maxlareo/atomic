18ab698357186ee125a0725decc2a8d694289d0e
Maxime Lareo <maxime@lareo.fr>
2024-05-13 22:02:26 +0200

    Update

--- config/scripts/1password.sh
+++ config/scripts/1password.sh
@@ -5,4 +5,5 @@ set -oue pipefail
 
 sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://downloads.1password.com/linux/keys/1password.asc" > /etc/yum.repos.d/1password.repo'
 
-rpm-ostree install 1password 1password-cli
+# rpm-ostree install 1password 1password-cli
+# 1Password will be installed in /opt which will be linked to /usr/lib/opt but for some reason the folder will be empty
